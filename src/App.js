896cc890e65ef0f5526a75e2933dde6853131fd1
ozkancondek <ozkncndek@gmail.com>
2021-12-22 10:35:39 +0100

    file structure

--- src/App.js
+++ src/App.js
@@ -10,24 +10,6 @@ import Instructors from "./pages/Instructors";
 import InstructorDetail from "./pages/InstructorDetail";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <MyNav />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/courses" element={<Courses />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/developing" element={<FullStack />} />
-        {/* For simplicity these two components didnt coded */}
-        <Route path="/data-science" element={<FullStack />} />
-        <Route path="/devops" element={<FullStack />} />
-        {/* ************************************************* */}
-        <Route path="/instructors" element={<Instructors />} />
-        <Route path="/instructors/:login" element={<InstructorDetail />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
-  );
+  return <div>my home</div>;
 }
 export default App;
